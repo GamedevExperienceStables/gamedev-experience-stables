f8748cf139d9db43f89120eac849aa09e17de22a
Alexander <alex.prozvitskiy@gmail.com>
2023-01-13 23:19:40 +0300

    [AP] Added editor tools (#29)

---
+++ Assets/Game/Scripts/Editor/Tools/PlayerPrefs.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Game.Editor.Tools
+{
+    public static class PlayerPrefs
+    {
+        [MenuItem("Tools/Player Prefs/Clear", priority = 10)]
+        public static void ClearPrefs()
+        {
+            UnityEngine.PlayerPrefs.DeleteAll();
+            UnityEngine.PlayerPrefs.Save();
+            
+            Debug.Log("Player Prefs cleared");
+        }
+    }
+}
