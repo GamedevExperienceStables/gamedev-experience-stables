469b1d94522b005c51ee034e6400181999bf2fd3
Alexander <alex.prozvitskiy@gmail.com>
2023-01-18 23:53:28 +0300

    added RandomService

---
+++ Assets/Game/Scripts/RandomManagement/RandomService.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Game.RandomManagement
+{
+    [UsedImplicitly]
+    public class RandomService
+    {
+        public Vector3 NextRandomInCircle(Vector3 position, float radius)
+        {
+            Vector2 value = GetRandomInsideUnitCircle() * radius;
+            return position + new Vector3(value.x, 0, value.y);
+        }
+
+        private static Vector2 GetRandomInsideUnitCircle() 
+            => Random.insideUnitCircle;
+    }
+}
