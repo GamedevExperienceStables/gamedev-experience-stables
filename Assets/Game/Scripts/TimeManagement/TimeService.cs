9bfa1bfd48e84a43109906963469beef0fb9d4fa
Alexander <alex.prozvitskiy@gmail.com>
2023-02-08 16:57:10 +0300

    [AP] Added local timers (#55)
    * added updatable timer
    
    * added timer callback and loop
    
    * removed unused time properties

--- Assets/Game/Scripts/TimeManagement/TimeService.cs
+++ Assets/Game/Scripts/TimeManagement/TimeService.cs
@@ -1,16 +1,18 @@
-﻿using UnityEngine;
-using VContainer;
+﻿using VContainer;
 
 namespace Game.TimeManagement
 {
-    public class TimeService
+    public class TimeService : ITimeProvider, ITimeService
     {
         [Inject]
         public TimeService()
         {
         }
 
-        public void Pause() => Time.timeScale = 0;
-        public void Play() => Time.timeScale = 1;
+        public void Pause() => UnityEngine.Time.timeScale = 0;
+        public void Play() => UnityEngine.Time.timeScale = 1;
+
+        public float WorldTime => UnityEngine.Time.time;
+        public float RealtimeSinceStartup => UnityEngine.Time.realtimeSinceStartup;
     }
 }
---
+++ Assets/Game/Scripts/TimeManagement/TimerFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using VContainer;
+
+namespace Game.TimeManagement
+{
+    public class TimerFactory
+    {
+        private readonly ITimeProvider _timeProvider;
+
+        [Inject]
+        public TimerFactory(ITimeProvider timeProvider)
+            => _timeProvider = timeProvider;
+
+        public TimerUpdatable CreateTimer(
+            TimeSpan duration,
+            Action onComplete = null,
+            bool isLooped = false,
+            bool ignoreTimeScale = false
+        )
+            => new(duration, onComplete, isLooped, ignoreTimeScale, _timeProvider);
+    }
+}
