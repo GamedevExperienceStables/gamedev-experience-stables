8a911e18893b7c51a03b9ce211b2fd6ec58964f3
Alexander <alex.prozvitskiy@gmail.com>
2023-02-10 20:59:57 +0300

    created readonly material data

--- Assets/Game/Scripts/Inventory/Items/Materials/IReadOnlyMaterialContainer.cs
+++ Assets/Game/Scripts/Inventory/Items/Materials/IReadOnlyMaterialContainer.cs
@@ -4,10 +4,12 @@ namespace Game.Inventory
     {
         void Subscribe(MaterialContainer.MaterialChangedEvent callback);
         void UnSubscribe(MaterialContainer.MaterialChangedEvent callback);
+
+        IReadOnlyMaterialData GetMaterialData(MaterialDefinition definition);
         
         int GetCurrentValue(MaterialDefinition definition);
         int GetTotalValue(MaterialDefinition definition);
-        
+
         bool IsFull(MaterialDefinition definition);
         bool IsEmpty(MaterialDefinition definition);
     }
