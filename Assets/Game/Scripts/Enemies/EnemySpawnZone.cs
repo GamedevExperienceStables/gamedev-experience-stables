9af900f30eee5bc32c9aa99c1ead70898eb2c97b
Arseniy Astashevich <ars.astashevich@gmail.com>
2023-02-13 22:29:33 +0300

    [AA] Fix

--- Assets/Game/Scripts/Enemies/EnemySpawnZone.cs
+++ Assets/Game/Scripts/Enemies/EnemySpawnZone.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using Game.TimeManagement;
 using UnityEngine;
+using UnityEngine.Serialization;
 using VContainer;
 
 namespace Game.Enemies
 {
     public class EnemySpawnZone : MonoBehaviour
     {
+        [FormerlySerializedAs("enemySpawnPoint")]
         [SerializeField]
         private List<EnemySpawnPoint> enemySpawnPoints;
 
