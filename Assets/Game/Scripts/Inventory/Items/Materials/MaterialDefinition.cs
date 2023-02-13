0bac92ef9aba2906599f4a40e3f8a6547d5cec81
Arseniy <88400039+Astashevich@users.noreply.github.com>
2023-02-13 21:02:39 +0300

    [AA] Update hud (#59)
    * [AA] Try hud
    
    * updated hud health container
    
    * added changes to health bar
    
    * [AA] Implement art design
    
    * [AP] Update HUD binding (#61)
    
    * added to material ui color
    
    * created readonly material data
    
    * added possibility to get current level material data
    
    * changed material ui binding
    
    * removed unused methods
    
    * [AA] Implement new hud functional
    
    ---------
    
    Co-authored-by: Alexander <alex.prozvitskiy@gmail.com>

--- Assets/Game/Scripts/Inventory/Items/Materials/MaterialDefinition.cs
+++ Assets/Game/Scripts/Inventory/Items/Materials/MaterialDefinition.cs
@@ -5,5 +5,9 @@ namespace Game.Inventory
     [CreateAssetMenu(menuName = MENU_PATH + "Crystal")]
     public class MaterialDefinition : ItemInventoryDefinition
     {
+        [SerializeField]
+        private Color color = Color.black;
+
+        public Color Color => color;
     }
 }
