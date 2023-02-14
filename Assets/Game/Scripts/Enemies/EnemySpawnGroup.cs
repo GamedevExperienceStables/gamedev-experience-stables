f762d3c23fd7988fc856d3486669bf8a90604621
Arseniy Astashevich <ars.astashevich@gmail.com>
2023-02-14 18:35:40 +0300

    [AA] Fixes, add prefabs

---
+++ Assets/Game/Scripts/Enemies/EnemySpawnGroup.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using Game.TimeManagement;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.Serialization;
+using VContainer;
+
+namespace Game.Enemies
+{
+    public class EnemySpawnGroup : MonoBehaviour
+    {
+        [SerializeField, ReadOnly]
+        private List<EnemySpawnPoint> enemySpawnPoints;
+
+        [SerializeField, Min(1)]
+        private int spawnCounts = 1;
+        [SerializeField, Min(0f)]
+        private float respawnTimer = 0f;
+
+        private float _timeSinceLastSpawn;
+        private Transform _target;
+        private bool _canSpawn = true;
+
+        private Transform _spawnContainer;
+        private TimerUpdatable _spawnTimer;
+        private bool _initialized;
+        
+        private void OnValidate()
+        {
+            enemySpawnPoints.Clear();
+            foreach (Transform child in transform)
+            {
+                if (child.TryGetComponent(out EnemySpawnPoint enemySpawnPoint))
+                    enemySpawnPoints.Add(enemySpawnPoint);
+            }
+        }
+
+        [Inject]
+        public void Construct(TimerFactory factory){
+            _spawnTimer = factory.CreateTimer(TimeSpan.FromSeconds(respawnTimer), OnCompleteSpawnTimer);
+        }
+
+        public void Init(Transform spawnContainer)
+        {
+            _initialized = true;
+            _spawnContainer = spawnContainer;
+        }
+
+        public void SetTarget(Transform target)
+            => _target = target;
+
+        public void Update()
+        {
+            if (!_initialized)
+                return;
+            
+            if (_canSpawn)
+            {
+                _spawnTimer.Stop();
+                return;
+            }
+            
+            _spawnTimer.Tick();
+            if (!CheckAllEnemiesDead()) 
+                RestartTimer();
+        }
+
+        public void Activate()
+        {
+            if (spawnCounts <= 0) 
+                return;
+            
+            if (!_canSpawn)
+                return;
+
+            spawnCounts--;
+            ActivateSpawnPoints();
+            _canSpawn = false;
+        }
+
+        private void RestartTimer()
+            => _spawnTimer.Start();
+
+        private bool CheckAllEnemiesDead()
+        {
+            bool allEnemiesDead = true;
+            foreach (EnemySpawnPoint spawnPoint in enemySpawnPoints)
+            {
+                allEnemiesDead &= spawnPoint.IsCleared;
+            }
+
+            return allEnemiesDead;
+        }
+
+
+        private void OnCompleteSpawnTimer()
+            => _canSpawn = true;
+
+        private void ActivateSpawnPoints()
+        {
+            enemySpawnPoints.ForEach(spawnPoint =>
+            {
+                spawnPoint.SetTarget(_target);
+                spawnPoint.Init(_spawnContainer);
+            });
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.yellow;
+
+            Vector3 position = transform.position;
+            foreach (EnemySpawnPoint spawnZone in enemySpawnPoints)
+                Gizmos.DrawLine(position, spawnZone.transform.position);
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (!_canSpawn) 
+                return;
+            
+            if (spawnCounts <= 0) 
+                return;
+            
+            Vector3 position = transform.position;
+            Gizmos.DrawIcon(new Vector3(position.x, 2f, position.z), "fire.png", false);
+        }
+    }
+}
--- Assets/Game/Scripts/Enemies/EnemySpawnZone.cs
+++ Assets/Game/Scripts/Enemies/EnemySpawnZone.cs
@@ -1,99 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-using Game.TimeManagement;
+using Game.Level;
+using NaughtyAttributes;
 using UnityEngine;
-using UnityEngine.Serialization;
-using VContainer;
 
 namespace Game.Enemies
 {
     public class EnemySpawnZone : MonoBehaviour
     {
-        [FormerlySerializedAs("enemySpawnPoint")]
-        [SerializeField]
-        private List<EnemySpawnPoint> enemySpawnPoints;
+        [SerializeField, ReadOnly]
+        private List<EnemySpawnGroup> enemySpawnZones;
 
-        [SerializeField, Min(1)]
-        private int spawnCounts = 1;
-        [SerializeField, Min(0f)]
-        private float respawnTimer = 0f;
-
-        private float _timeSinceLastSpawn;
-        private Transform _target;
-        private bool _canSpawn = true;
-
-        private Transform _spawnContainer;
-        private TimerUpdatable _spawnTimer;
-        private bool _initialized;
-
-        [Inject]
-        public void Construct(TimerFactory factory){
-            _spawnTimer = factory.CreateTimer(TimeSpan.FromSeconds(respawnTimer), OnCompleteSpawnTimer);
-        }
-
-        public void Init(Transform spawnContainer)
+        private void OnValidate()
         {
-            _initialized = true;
-            _spawnContainer = spawnContainer;
-        }
-
-        public void SetTarget(Transform target)
-            => _target = target;
-
-        public void Update()
-        {
-            if (!_initialized)
-                return;
-            
-            if (_canSpawn)
+            enemySpawnZones.Clear();
+            foreach (Transform child in transform)
             {
-                _spawnTimer.Stop();
-                return;
+                if (child.TryGetComponent(out EnemySpawnGroup enemySpawnGroup))
+                    enemySpawnZones.Add(enemySpawnGroup);
             }
-            
-            _spawnTimer.Tick();
-            if (!CheckAllEnemiesDead()) 
-                RestartTimer();
         }
 
-        public void Activate()
+        public void OnTriggerEnter(Collider other)
         {
-            if (spawnCounts <= 0) 
+            if (!other.TryGetComponent(out ISpawnZoneTrigger _))
                 return;
             
-            if (!_canSpawn)
-                return;
-
-            spawnCounts--;
-            ActivateSpawnPoints();
-            _canSpawn = false;
-        }
-
-        private void RestartTimer()
-            => _spawnTimer.Start();
-
-        private bool CheckAllEnemiesDead()
-        {
-            bool allEnemiesDead = true;
-            foreach (EnemySpawnPoint spawnPoint in enemySpawnPoints)
+            foreach (EnemySpawnGroup enemySpawnZone in enemySpawnZones)
             {
-                allEnemiesDead &= spawnPoint.IsCleared;
+                enemySpawnZone.Activate();
             }
-
-            return allEnemiesDead;
-        }
-
-
-        private void OnCompleteSpawnTimer()
-            => _canSpawn = true;
-
-        private void ActivateSpawnPoints()
-        {
-            enemySpawnPoints.ForEach(spawnPoint =>
-            {
-                spawnPoint.SetTarget(_target);
-                spawnPoint.Init(_spawnContainer);
-            });
         }
 
         private void OnDrawGizmosSelected()
@@ -101,20 +37,8 @@ namespace Game.Enemies
             Gizmos.color = Color.yellow;
 
             Vector3 position = transform.position;
-            foreach (EnemySpawnPoint spawnZone in enemySpawnPoints)
+            foreach (EnemySpawnGroup spawnZone in enemySpawnZones)
                 Gizmos.DrawLine(position, spawnZone.transform.position);
-        }
-
-        private void OnDrawGizmos()
-        {
-            if (!_canSpawn) 
-                return;
-            
-            if (spawnCounts <= 0) 
-                return;
-            
-            Vector3 position = transform.position;
-            Gizmos.DrawIcon(new Vector3(position.x, 2f, position.z), "fire.png", false);
         }
     }
 }
