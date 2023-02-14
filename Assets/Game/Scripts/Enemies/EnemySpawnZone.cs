9280329e5d4ce34af3310280a1605693fe03ab0c
Arseniy <88400039+Astashevich@users.noreply.github.com>
2023-02-14 21:01:56 +0300

    [AA] Update spawn system (#65)
    * [AA] Update spawn system
    
    * [AA] Implement timer in spawner
    
    * [AA] Fixes
    
    * [AA] Fixes
    
    * [AA] Fix
    
    * fixed spawn zone null exception
    
    * made same field initialization naming as spawn zone
    
    * moved the check logic inside spawn point
    
    * moved enemiesLeft assigning into init method
    
    * fixed double spawning if spawnCount == 1
    
    * [AA] Fixes, add prefabs
    
    * revert spawn zone changes
    
    * was hidden spawn list in inspector
    
    * added to spawn points required attribute
    
    * formatted
    
    * added to spawn points inspector attributes
    
    ---------
    
    Co-authored-by: Alexander <alex.prozvitskiy@gmail.com>

--- Assets/Game/Scripts/Enemies/EnemySpawnZone.cs
+++ Assets/Game/Scripts/Enemies/EnemySpawnZone.cs
@@ -1,64 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Game.Level;
-using MoreMountains.Feedbacks;
 using UnityEngine;
-using UnityEngine.Serialization;
-using VContainer;
 
 namespace Game.Enemies
 {
     public class EnemySpawnZone : MonoBehaviour
     {
-        [SerializeField]
-        private List<EnemySpawnPoint> enemySpawnPoint;
+        [SerializeField, HideInInspector]
+        private List<EnemySpawnGroup> enemySpawnZones;
 
-        [SerializeField, Min(1f)]
-        private float spawnCounts = 1f;
-        [SerializeField, Min(0f)]
-        private float respawnTimer = 0f;
-
-        private float _timeSinceLastSpawn;
-        private Transform _target;
-        private bool _hasTarget;
-
-        private Transform _spawnContainer;
-
-        public void Init(Transform spawnContainer)
-            => _spawnContainer = spawnContainer;
-
-        public void SetTarget(Transform target)
+        private void OnValidate()
         {
-            _target = target;
-            _hasTarget = true;
+            enemySpawnZones.Clear();
+            foreach (Transform child in transform)
+            {
+                if (child.TryGetComponent(out EnemySpawnGroup enemySpawnGroup))
+                    enemySpawnZones.Add(enemySpawnGroup);
+            }
         }
 
-        public void Update()
+        public void OnTriggerEnter(Collider other)
         {
-            UpdateTimer();
-        }
-
-        private void UpdateTimer()
-        {
-            //TODO: Написать обработчик после зачистки зоны запустить таймер для повторного запуска зоны
-        }
-
-        public void Activate()
-        {
-            if (spawnCounts <= 0) 
+            if (!other.TryGetComponent(out ISpawnZoneTrigger _))
                 return;
 
-            spawnCounts -= 1f;
-            ActivateSpawnPoints();
-        }
-
-        private void ActivateSpawnPoints()
-        {
-            enemySpawnPoint.ForEach(spawnPoint =>
+            foreach (EnemySpawnGroup enemySpawnZone in enemySpawnZones)
             {
-                spawnPoint.Init(_spawnContainer);
-                spawnPoint.SetTarget(_target);
-            });
+                enemySpawnZone.Activate();
+            }
         }
 
         private void OnDrawGizmosSelected()
@@ -66,7 +35,7 @@ namespace Game.Enemies
             Gizmos.color = Color.yellow;
 
             Vector3 position = transform.position;
-            foreach (EnemySpawnPoint spawnZone in enemySpawnPoint)
+            foreach (EnemySpawnGroup spawnZone in enemySpawnZones)
                 Gizmos.DrawLine(position, spawnZone.transform.position);
         }
     }
