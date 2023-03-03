0283a2190be76f69d444f1fb809b912685f02602
Alexander <alex.prozvitskiy@gmail.com>
2023-03-04 01:58:23 +0300

    editor: created menu item for force script recompilation

---
+++ Assets/Game/Scripts/Editor/Tools/RecompileScripts.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEditor.Compilation;
+
+namespace Game.Editor.Tools
+{
+    public static class RecompileScripts
+    {
+        private const string MENU_PATH = "Tools/Editor/Force Recompile";
+
+        [MenuItem(MENU_PATH, true)]
+        public static bool Validate()
+            => !EditorApplication.isPlaying;
+
+        [MenuItem(MENU_PATH, false)]
+        public static void OpenFolderPersistentDataPath()
+            => CompilationPipeline.RequestScriptCompilation();
+    }
+}
