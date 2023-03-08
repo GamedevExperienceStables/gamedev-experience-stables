4f0bb33e531740a968b83aaa60e063bb8f0fe236
aii <aii@devkeeper.ru>
2023-03-09 02:25:58 +0400

    lagging pet 1st iteration

--- Assets/Game/Level/Data/Pet/PetFollowing.cs
+++ Assets/Game/Level/Data/Pet/PetFollowing.cs
@@ -6,8 +6,14 @@ namespace Game.Level.Data.Pet
     public class PetFollowing : MonoBehaviour
     {
         [SerializeField]
-        private Transform heroPosition;
+        private Transform followingPosition;
+
+        [SerializeField]
+        private float speed;
         private Vector3 _petPosition;
+        private bool _isChasing;
+        
+
         private void Start()
         {
             _petPosition = transform.position;
@@ -15,7 +21,27 @@ namespace Game.Level.Data.Pet
 
         void Update()
         {
-            transform.position = _petPosition;
+            Chasing(_isChasing);
+            if (!_isChasing && Vector3.Distance(transform.position, followingPosition.position) > 2)
+            {
+                _isChasing = true;
+            }
+            else if (_isChasing && Vector3.Distance(transform.position, followingPosition.position) < 0.2)
+            {
+                _isChasing = false;
+            }
+            else
+            {
+                transform.position = _petPosition;
+            }
+        }
+
+        private void Chasing(bool isChasing)
+        {
+            if (!isChasing) return;
+            transform.position = Vector3.MoveTowards(transform.position, followingPosition.position,
+                speed * Time.deltaTime);
+            _petPosition = transform.position;
         }
     }
 }
