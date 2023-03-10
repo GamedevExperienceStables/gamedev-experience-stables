cb365c48dc1b3803eddef2e8d6d7615ab2bbec7b
Alexander <alex.prozvitskiy@gmail.com>
2023-03-11 02:40:03 +0300

    fixup! layers: added ignore player layer

--- Assets/Game/Scripts/Utils/LayerMasks.cs
+++ Assets/Game/Scripts/Utils/LayerMasks.cs
@@ -4,6 +4,7 @@ namespace Game.Utils
 {
     public static class LayerMasks
     {
+        public static readonly LayerMask Default = 1 << 0;
         public static readonly LayerMask Player = 1 << 3;
         public static readonly LayerMask Npc = 1 << 6;
         public static readonly LayerMask Projectile = 1 << 7;
