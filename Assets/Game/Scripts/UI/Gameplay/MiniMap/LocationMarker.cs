468b22a1bfc4c8ac6759add9c836ed32659a7f23
Alexander <alex.prozvitskiy@gmail.com>
2023-03-20 23:16:28 +0300

    implemented markers on mini map

---
+++ Assets/Game/Scripts/UI/Gameplay/MiniMap/LocationMarker.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using VContainer;
+
+namespace Game.UI
+{
+    public class LocationMarker : MonoBehaviour, ILocationMarker
+    {
+        [SerializeField]
+        private MapMarkerDefinition definition;
+
+        private Transform _transform;
+        private LocationMarkers _markers;
+
+        public Sprite Icon => definition.Icon;
+        public Vector3 Position => _transform.position;
+
+        [Inject]
+        public void Construct(LocationMarkers markers)
+            => _markers = markers;
+
+        private void Awake()
+            => _transform = GetComponent<Transform>();
+
+        private void Start()
+            => _markers?.Add(this);
+
+        private void OnDestroy()
+            => _markers?.Remove(this);
+    }
+}
---
+++ Assets/Game/Scripts/UI/Gameplay/MiniMap/MiniMapMarkers.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Game.UI
+{
+    public class MiniMapMarkers
+    {
+        private readonly List<MapMarker> _markers = new();
+        private readonly MiniMap _map;
+
+        public MiniMapMarkers(MiniMap map)
+            => _map = map;
+
+        public void Create(ILocationMarker locationMarker)
+        {
+            MapMarker marker = CreateMarker(locationMarker);
+
+            _map.Add(marker.Element);
+            _markers.Add(marker);
+
+            // move marker under hero marker
+            marker.Element.SendToBack();
+        }
+
+        public void Remove(ILocationMarker locationMarker)
+        {
+            MapMarker marker = _markers.Find(m => m.Is(locationMarker));
+            if (marker is null)
+                return;
+
+            _map.Remove(marker.Element);
+            _markers.Remove(marker);
+        }
+
+        public void Tick()
+        {
+            for (int i = _markers.Count - 1; i >= 0; i--)
+                UpdateMarker(_markers[i]);
+        }
+
+        private static MapMarker CreateMarker(ILocationMarker target)
+        {
+            var icon = new Image { sprite = target.Icon };
+            icon.AddToClassList(LayoutNames.MiniMap.MARKER_CLASS_NAME);
+
+            var marker = new MapMarker(target, icon);
+            return marker;
+        }
+
+        private void UpdateMarker(MapMarker marker)
+        {
+            if (!marker.IsWorldPositionChanged())
+                return;
+
+            Vector2 mapPosition = _map.WorldToMapPosition(marker.WorldPosition);
+            marker.UpdatePosition(mapPosition);
+        }
+
+        public void Clear()
+        {
+            foreach (MapMarker marker in _markers)
+                _map.Remove(marker.Element);
+
+            _markers.Clear();
+        }
+    }
+}
