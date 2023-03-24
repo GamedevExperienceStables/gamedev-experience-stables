29f8aed98133836bf11f7751d257a0e49b615d00
Alexander <alex.prozvitskiy@gmail.com>
2023-03-24 03:16:58 +0300

    added simplified intro cutscene

---
+++ Assets/Game/Scripts/GameFlow/Intro/IntroController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using VContainer;
+
+namespace Game.GameFlow
+{
+    public class IntroController : MonoBehaviour
+    {
+        private RootStateMachine _rootStateMachine;
+
+        [Inject]
+        public void Construct(RootStateMachine rootStateMachine)
+            => _rootStateMachine = rootStateMachine;
+
+
+        public void Complete()
+            => _rootStateMachine.EnterState<PlanetState>();
+    }
+}
