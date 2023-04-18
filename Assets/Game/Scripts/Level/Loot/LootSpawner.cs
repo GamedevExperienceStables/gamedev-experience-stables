a5b0b5b7ac39c8e0874980fe1a4a1c193ac47ad8
Alexander <alex.prozvitskiy@gmail.com>
2023-04-18 23:04:41 +0300

    [AP] Implemented loot tables (#140)
    * implemented loot tables
    
    * fixed empty loot

--- Assets/Game/Scripts/Level/Loot/LootSpawner.cs
+++ Assets/Game/Scripts/Level/Loot/LootSpawner.cs
@@ -1,15 +1,18 @@
-﻿using Game.RandomManagement;
+﻿using System.Collections.Generic;
+using Game.RandomManagement;
 using Game.Settings;
 using UnityEngine;
 using VContainer;
 
 namespace Game.Level
 {
-    public class LootSpawner 
+    public class LootSpawner
     {
         private readonly LootFactory _factory;
         private readonly RandomService _random;
         private readonly LootSettings _settings;
+
+        private readonly List<LootDefinitionItem> _bufferItems = new();
 
         [Inject]
         public LootSpawner(LootFactory factory, RandomService random, LootSettings settings)
@@ -21,14 +24,48 @@ namespace Game.Level
 
         public void SpawnScattered(LootBagDefinition lootBagDefinition, Vector3 position)
         {
-            foreach (LootDefinitionItem itemDefinition in lootBagDefinition.Items)
+            FetchLoot(lootBagDefinition);
+
+            foreach (LootDefinitionItem definition in _bufferItems)
             {
-                for (int i = 0; i < itemDefinition.Count; i++)
-                {
-                    Vector3 spawnPosition = _random.NextRandomInCircle(position, _settings.SpawnScatterRadius);
-                    SpawnItem(spawnPosition, itemDefinition);
-                }
+                Vector3 spawnPosition = _random.NextRandomInCircle(position, _settings.SpawnScatterRadius);
+                SpawnItem(spawnPosition, definition);
             }
+
+            _bufferItems.Clear();
+        }
+
+        private void FetchLoot(LootBagDefinition lootBagDefinition)
+        {
+            foreach (LootTableDefinitionItem lootTable in lootBagDefinition.Tables)
+            {
+                lootTable.ComputeWeights();
+
+                if (TryGetLootFromTable(lootTable, out LootDefinitionItem definition))
+                    AddItem(definition);
+            }
+
+            foreach (LootDefinitionItem definition in lootBagDefinition.Items)
+                AddItem(definition);
+        }
+
+        private void AddItem(LootDefinitionItem definition)
+        {
+            for (int i = 0; i < definition.Count; i++)
+                _bufferItems.Add(definition);
+        }
+
+        private static bool TryGetLootFromTable(LootTableDefinitionItem lootTable, out LootDefinitionItem itemDefinition)
+        {
+            LootTableItem lootTableItem = lootTable.GetLoot();
+            if (lootTableItem.Loot?.Definition)
+            {
+                itemDefinition = lootTableItem.Loot;
+                return true;
+            }
+
+            itemDefinition = null;
+            return false;
         }
 
         private void SpawnItem(Vector3 position, LootDefinitionItem item)
