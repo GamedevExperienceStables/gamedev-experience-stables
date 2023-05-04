28b6e1290fcae0ecbbaf7817b13bda0c90a81203
Alexander <alex.prozvitskiy@gmail.com>
2023-05-04 20:38:01 +0300

    added water zone and splashes

---
+++ Assets/Game/Scripts/Level/ZoneWater.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Game.Level
+{
+    [RequireComponent(typeof(ZoneTrigger))]
+    public class ZoneWater : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject enterEffect;
+
+        private ZoneTrigger _trigger;
+
+        private void Awake()
+            => _trigger = GetComponent<ZoneTrigger>();
+
+        private void OnEnable()
+            => _trigger.TriggerEntered += OnTriggerEntered;
+
+        private void OnDisable()
+            => _trigger.TriggerEntered -= OnTriggerEntered;
+
+        private void OnTriggerEntered(GameObject other)
+            => Instantiate(enterEffect, other.transform.position, Quaternion.identity);
+    }
+}
