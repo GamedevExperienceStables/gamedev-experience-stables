cfa1dded9335276b13932ef333859c8c9aba28fe
Alexander <alex.prozvitskiy@gmail.com>
2023-05-20 21:15:17 +0300

    moved save files to own directory

--- Assets/Game/Scripts/Persistence/LocalPersistence.cs
+++ Assets/Game/Scripts/Persistence/LocalPersistence.cs
@@ -8,17 +8,18 @@ namespace Game.Persistence
     public class LocalPersistence : IPersistence
     {
         private readonly IDataSerializer _serializer;
+        private const string DIRECTORY = "Saves";
 
         [Inject]
         public LocalPersistence(IDataSerializer serializer)
             => _serializer = serializer;
 
         public bool Exists(string filename)
-            => File.Exists(GetFilePath(filename));
+            => File.Exists(GetOrCreateFilePath(filename));
 
         public async UniTask SerializeAsync<T>(T data, string filename)
         {
-            string filepath = GetFilePath(filename);
+            string filepath = GetOrCreateFilePath(filename);
 
             await using var sw = new StreamWriter(filepath, false);
             await _serializer.SerializeAsync(data, sw);
@@ -26,7 +27,7 @@ namespace Game.Persistence
 
         public async UniTask<T> DeserializeAsync<T>(string filename)
         {
-            string filepath = GetFilePath(filename);
+            string filepath = GetOrCreateFilePath(filename);
 
             if (!File.Exists(filepath))
                 throw new FileNotFoundException($"There is no file at the path \"{filepath}\".");
@@ -38,7 +39,7 @@ namespace Game.Persistence
 
         public bool Delete(string filename)
         {
-            string filepath = GetFilePath(filename);
+            string filepath = GetOrCreateFilePath(filename);
 
             if (!File.Exists(filepath))
                 return false;
@@ -47,7 +48,14 @@ namespace Game.Persistence
             return true;
         }
 
-        private static string GetFilePath(string filename)
-            => Path.Combine(Application.persistentDataPath, filename);
+        private static string GetOrCreateFilePath(string filename)
+        {
+            string dirPath = Path.Combine(Application.persistentDataPath, DIRECTORY);
+            var dir = new DirectoryInfo(Path.GetFullPath(dirPath));
+            if (!dir.Exists)
+                dir.Create();
+
+            return Path.Combine(dirPath, filename);
+        }
     }
 }
